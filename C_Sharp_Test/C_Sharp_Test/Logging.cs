de8f8f0e89f514b61c32f6641ce2a99280bc6e04
MorganBernhardt <62308940+MorganBernhardt@users.noreply.github.com>
2020-03-17 22:13:56 +0000

    Add files via upload

---
+++ C_Sharp_Test/C_Sharp_Test/Logging.cs
@@ -0,0 +1,73 @@
+﻿
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace C_Sharp_Test
+    {
+    class Logging
+    {
+        public static void Write(string Content, string path, string sFileName)
+        {
+            if (File.Exists(FolderPls(path) + sFileName))
+            {
+                File.Delete(FolderPls(path) + sFileName);
+
+            }
+            LogFilePls(path, sFileName, Content);
+        }
+        public static void Write(string Log_Content)
+        {
+            string Enhanced_Log_Content = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + Log_Content;
+            string path = System.Reflection.Assembly.GetEntryAssembly().Location;
+            path = path.Substring(0, path.LastIndexOf("\\") + 1);
+            LogFilePls(path + "log\\", DateTime.Now.ToString("yyyyMMdd_") + "Log.txt", Enhanced_Log_Content);
+        }
+        private static void LogFilePls(string FolderPath, string LogFileName, string Enhanced_Log_Content)
+        {
+            try
+            {
+                if (File.Exists(FolderPls(FolderPath) + LogFileName))
+                {
+                    using (TextWriter tw = new StreamWriter(FolderPath + LogFileName, true))
+                    {
+                        tw.WriteLine(Enhanced_Log_Content);
+                        tw.Close();
+                    }
+                }
+                else
+                {
+                    File.Create(FolderPath + LogFileName).Close();
+                    using (TextWriter tw = new StreamWriter(FolderPath + LogFileName, true))
+                    {
+                        tw.WriteLine(Enhanced_Log_Content);
+                        tw.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private static string FolderPls(string FolderPath)
+        {
+            try
+            {
+                if (!Directory.Exists(FolderPath))
+                {
+                    Directory.CreateDirectory(FolderPath);
+                }
+                return FolderPath;
+            }
+            catch (Exception ex)
+            {
+                return FolderPath;
+            }
+        }
+
+    }
+}
--- C_Sharp_Test/C_Sharp_Test/cls_model.cs
+++ C_Sharp_Test/C_Sharp_Test/cls_model.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Test
 {
-    public class cls_model_row
+     public class cls_model_row
     {
         public int paymentid { get; set; }
         public string currency { get; set; }
@@ -16,14 +16,18 @@ namespace C_Sharp_Test
 
 
 
-        public static cls_model_row frmAPI(int _paymentid, string _currency, double _amount, DateTime _paymentDate, bool _settled)
+        public static cls_model_row frmAPI(string csvline)
         {
             cls_model_row mr = new cls_model_row();
-            mr.paymentid = _paymentid;
-            mr.currency = _currency;
-            mr.amount = _amount;
-            mr.paymentDate = _paymentDate;
-            mr.settled = _settled;
+            string[] values = csvline.Split(',');
+            if (values.Length == 5)
+            {                
+                mr.paymentid = Convert.ToInt16(values[0]);
+                mr.currency = Convert.ToString(values[1]);
+                mr.amount = Convert.ToDouble(values[2]);
+                mr.paymentDate = Convert.ToDateTime(values[3]);
+                mr.settled = Convert.ToBoolean(values[4]);                
+            }
             return mr;
         }
     }
